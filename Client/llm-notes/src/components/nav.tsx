d2414d2ab147864ae55025eb1e83ef13cf97ef2f
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-04 19:28:56 -0700

    Deleted boilerplate

---
+++ Client/llm-notes/src/components/nav.tsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <div className="fixed">
+      <div>
+        <h1>LLM Notes</h1>
+      </div>
+      <div>
+        <button>New Note</button>
+      </div>
+    </div>
+  );
+}
