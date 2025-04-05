8f8bbfc2635f7f0f755add147d0755296c3c849d
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-04 19:47:14 -0700

    implemented simple unique nav

--- Client/llm-notes/src/components/nav.tsx
+++ Client/llm-notes/src/components/nav.tsx
@@ -1,11 +1,10 @@
 export default function Nav() {
   return (
-    <div className="fixed">
+    <div className="fixed right-0 left-[50%] top-[7%] translate-[-50%] bg-[#282828] flex justify-center gap-5 w-[80%] p-5 rounded-4xl">
       <div>
-        <h1>LLM Notes</h1>
-      </div>
-      <div>
-        <button>New Note</button>
+        <button className="border-2 border-[#575757] p-2 rounded-2xl cursor-pointer">
+          New Note
+        </button>
       </div>
     </div>
   );
