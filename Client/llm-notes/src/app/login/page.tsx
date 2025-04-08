b2a8419c2cf9fdca82973272378ea37240eaa1dc
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-07 22:34:49 -0700

    implemented basic OAuth login page!

--- Client/llm-notes/src/app/login/page.tsx
+++ Client/llm-notes/src/app/login/page.tsx
@@ -1,16 +1,32 @@
-import { Suspense } from "react";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function LoginPage() {
-  return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-          </div>
-        </div>
-        <Suspense>
-        </Suspense>
-      </div>
-    </main>
+export default async function SignIn() {
+  const session = await auth();
+  console.log(session);
+  const user = session?.user;
+
+  return user ? (
+    <>
+      <h1>Welcome {user.name}</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}>
+        <button className="cursor-pointer p-2 border-2 bg-blue-400 text-black" type="submit">
+          Sign Out
+        </button>
+      </form>
+    </>
+  ) : (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}>
+      <button className="cursor-pointer p-2 border-2 bg-blue-400 text-black" type="submit">
+        Signin with Google
+      </button>
+    </form>
   );
 }
