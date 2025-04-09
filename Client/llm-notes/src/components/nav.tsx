a9ac8f5c02063dc08a30fd1f04d3079ebc58956c
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-08 21:44:09 -0700

    installed shadcn ui

--- Client/llm-notes/src/components/nav.tsx
+++ Client/llm-notes/src/components/nav.tsx
@@ -9,6 +9,9 @@ export default function Nav() {
   const redirectHome = () => {
     redirect("/");
   };
+  const redirectAccount = () => {
+    redirect("/account");
+  };
 
   return (
     <div className="fixed right-0 left-[50%] top-[7%] translate-[-50%] bg-[#282828] flex justify-center gap-5 w-[80%] p-5 rounded-4xl">
@@ -23,6 +26,11 @@ export default function Nav() {
         onClick={redirectNoteEditor}
         className="border-2 border-[#575757] p-2 rounded-2xl cursor-pointer">
         New Note
+      </button>
+      <button
+        onClick={redirectAccount}
+        className="border-2 border-[#575757] p-2 rounded-2xl cursor-pointer">
+        Account
       </button>
     </div>
   );
